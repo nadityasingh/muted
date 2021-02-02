5825e398df092bd0e8a293ddac45082125446473
nadityasingh <52741798+nadityasingh@users.noreply.github.com>
2021-02-02 17:03:07 +0530

    Add files via upload

---
+++ adding.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1 = 2, num2 = 2, num3;
+	
+	cout << "enter the two integer";
+	cin >> num1 >> num2;
+	num3  = num1 + num2;
+	
+	cout << "num1 << '+' << 2 '='<< num3";
+	return 0;
+}
