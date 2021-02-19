202cb42927cf5c6a6a8afa1fe0abee46910a9ff5
nadityasingh <52741798+nadityasingh@users.noreply.github.com>
2021-02-19 15:49:25 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,10 @@
+import calculator
+
+result1 = calculator.add(2,3)
+result2 = calculator.sub(2,3)
+result3 = calculator.mul(2,3)
+result4 = calculator.div(2,3)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ mod.py
@@ -0,0 +1,4 @@
+import math
+
+num = 25
+print(math.sqrt(25))
